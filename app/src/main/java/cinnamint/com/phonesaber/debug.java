4c3e41087e19fb230ed883c4329c0994451dd1b7
Cathy Cushy <hakuna.matata.kitty@gmail.com>
2016-01-10 11:15:58 -0600

    Commit 1.1 (Beta)

---
+++ app/src/main/java/cinnamint/com/phonesaber/debug.java
@@ -0,0 +1,38 @@
+package cinnamint.com.phonesaber;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by Gaga on 1/10/2016.
+ */
+public class debug extends Service {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(new receiver(), filter);
+
+        // start grabbing
+        Log.d("WINSTON", "DEBUG SERVICE CREATED");
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(getApplicationContext(), "RADIONUMBER" + MainActivity.SFX_option, Toast.LENGTH_LONG).show();
+        Log.d("WINSTON", "oSC DEBUGGGGG");
+        return START_REDELIVER_INTENT;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
