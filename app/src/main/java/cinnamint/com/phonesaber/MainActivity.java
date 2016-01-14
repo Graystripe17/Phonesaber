f70e547651247a2be384d9697e30c208bc67437d
Cathy Cushy <hakuna.matata.kitty@gmail.com>
2016-01-13 22:46:11 -0600

    Polished UX, logo in processing

--- app/src/main/java/cinnamint/com/phonesaber/MainActivity.java
+++ app/src/main/java/cinnamint/com/phonesaber/MainActivity.java
@@ -1,9 +1,7 @@
 package cinnamint.com.phonesaber;
 
-import android.annotation.TargetApi;
+import android.app.Activity;
 import android.app.AlarmManager;
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -11,27 +9,25 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
-import android.os.Build;
 import android.os.SystemClock;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     public Context context;
     public static final String MY_PREFS_NAME = "MyPrefsFile";
     public static int SFX_option;
-    private int count = 0;
+    public static boolean LowMemoryMode;
+    public BroadcastReceiver screenBR;
     /*
     0 Star Wars
     1 Doctor Who
@@ -39,14 +35,10 @@ public class MainActivity extends ActionBarActivity {
     3 Indiana Jones
     4 Harry Potter
     5 Zelda
-    6 Thor
+    6 Iron Man
     */
     RadioGroup radioGroup;
-    int mNotificationId = 001;
-    NotificationCompat.Builder mBuilder;
-    NotificationManager mNotifyMgr;
-    Notification note8;
-    Notification note11;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,8 +50,8 @@ public class MainActivity extends ActionBarActivity {
         radioGroup = (RadioGroup) findViewById(R.id.myOptions);
 
 
-        // Grab the most recent SFX_option
         SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        // Grab the most recent SFX_option
         if (prefs != null) {
             // If no resourceNumber exists, default phonesaber
             int resourceNumber = prefs.getInt("resourceNumber", 0);
@@ -68,16 +60,48 @@ public class MainActivity extends ActionBarActivity {
             // On start, set the default phoneSaber settings to 0
             ((RadioButton) radioGroup.getChildAt(resourceNumber)).setChecked(true);
         }
+        // Grab the most recent toggle option
+        if (prefs != null) {
+            // By default, LowMemoryMode is off
+            LowMemoryMode = prefs.getBoolean("LowMemoryMode", false);
+
+            ToggleButton LowMemoryModeButton = (ToggleButton)findViewById(R.id.LowMemoryToggle);
+            LowMemoryModeButton.setChecked(LowMemoryMode);
+
+            if(LowMemoryMode) {
+                stopHighMemoryMode();
+                startLowMemoryMode();
+            } else {
+                stopLowMemoryMode();
+                startHighMemoryMode();
+            }
+        }
+
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.LowMemoryToggle);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+                    editor.putBoolean("LowMemoryMode", true);
+                    editor.commit();
+                    stopHighMemoryMode();
+                    startLowMemoryMode();
+                } else {
+                    SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+                    editor.putBoolean("LowMemoryMode", false);
+                    editor.commit();
+                    startHighMemoryMode();
+                    // Be wary of order
+                    stopLowMemoryMode();
+                }
+            }
+        });
 
 
-        // USING receiver.java
-        // Receiver continues
-        IntentFilter filterDesiredActions = new IntentFilter();
-        filterDesiredActions.addAction(Intent.ACTION_SCREEN_ON);
-        filterDesiredActions.addAction(Intent.ACTION_SCREEN_OFF);
-        registerReceiver(new receiver(), filterDesiredActions);
 
-
+/**
+ * User Present
+ */
         // HANDLES ONLY USER_PRESENT
         // Receiver killed onTaskRemoved
         BroadcastReceiver UnlockReceiver = new BroadcastReceiver() {
@@ -106,66 +130,6 @@ public class MainActivity extends ActionBarActivity {
 
         IntentFilter filter = new IntentFilter(Intent.ACTION_USER_PRESENT);
         registerReceiver(UnlockReceiver, filter);
-
-/**
- * LOW MEMORY MODE
-// */
-//        // Alarm restarts service
-//        Intent restartService = new Intent(context, UpdateService.class);
-//        restartService.setPackage(getPackageName());
-//        PendingIntent restartServicePI = PendingIntent.getService(context, 1, restartService, PendingIntent.FLAG_ONE_SHOT);
-//
-//        // Restart OFTEN
-//        AlarmManager alarmService = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-//        alarmService.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 100, restartServicePI);
-//        alarmService.cancel(restartServicePI);
-
-/**
- * HIGH MEMORY MODE
- */
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            // FOREGROUND Notification
-            mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            note11 = new Notification.Builder(context)
-                    .setContentTitle("YEE HAW")
-                    .setContentText("MICHAEL JACKSON - SHAMONE")
-                    .setSmallIcon(R.drawable.notification_template_icon_bg).build();
-            mNotifyMgr.notify(mNotificationId, note11);
-        }
-        else {
-            note8 =
-                    new Notification(R.drawable.pepe, getString(R.string.noticeMe),
-                            System.currentTimeMillis());
-
-            PendingIntent i=PendingIntent.getActivity(this, 0,
-                    new Intent(this, UpdateService.class),
-                    0);
-            note8.setLatestEventInfo(getApplicationContext(), getString(R.string.title), getString(R.string.message), i);
-            note8.number=++count;
-            note8.flags |= Notification.FLAG_AUTO_CANCEL;
-            note8.flags |= Notification.DEFAULT_SOUND;
-            note8.flags |= Notification.DEFAULT_VIBRATE;
-            note8.ledARGB = 0xff0000ff;
-            note8.flags |= Notification.FLAG_SHOW_LIGHTS;
-
-            // Now invoke the Notification Service
-            String notifService = Context.NOTIFICATION_SERVICE;
-            NotificationManager mgr =
-                    (NotificationManager) getSystemService(notifService);
-            mgr.notify(mNotificationId, note8);
-        }
-
-
-        // DEBUG PURPOSES
-        /*
-        Intent debug = new Intent(context, debug.class);
-        debug.setPackage(getPackageName());
-        PendingIntent debugPI = PendingIntent.getService(context, 1, debug, PendingIntent.FLAG_ONE_SHOT);
-
-        AlarmManager debugAlarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        debugAlarm.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 100, 1000, debugPI);
-        */
-        // DEBUG PURPOSES
 
 
 
@@ -240,5 +204,47 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startHighMemoryMode() {
+//        Toast.makeText(context, "startHighMemoryMode", Toast.LENGTH_LONG).show();
+//
+//        Intent notificationIntent = new Intent(this, UpdateService.class);
+//        startService(notificationIntent);
+    }
+
+    public void stopHighMemoryMode() {
+        //stopService(new Intent(this, UpdateService.class));
+    }
+
+    public void startLowMemoryMode() {
+        Toast.makeText(context, "startLowMemoryMode", Toast.LENGTH_LONG).show();
+//        // USING receiver.java
+//        // Receiver continues
+        IntentFilter filterDesiredActions = new IntentFilter();
+        filterDesiredActions.addAction(Intent.ACTION_SCREEN_ON);
+        filterDesiredActions.addAction(Intent.ACTION_SCREEN_OFF);
+        screenBR = new receiver();
+        registerReceiver(screenBR, filterDesiredActions);
+
+
+
+        // Alarm restarts service
+        Intent restartService = new Intent(context, UpdateService.class);
+        restartService.setPackage(getPackageName());
+        PendingIntent restartServicePI = PendingIntent.getService(context, 1, restartService, PendingIntent.FLAG_ONE_SHOT);
+
+        // Restart OFTEN
+        AlarmManager alarmService = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmService.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 100, restartServicePI);
+        alarmService.cancel(restartServicePI);
+    }
+
+    public void stopLowMemoryMode() {
+        try {
+            unregisterReceiver(screenBR);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
     }
 }
