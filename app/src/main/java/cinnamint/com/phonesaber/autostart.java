2b056d3092eb8c074c82b8d90d396fd211a22cdf
Cathy Cushy <hakuna.matata.kitty@gmail.com>
2016-06-23 23:39:49 -0500

    Trying to reinstate autostart. CATACLYYSSSM

--- app/src/main/java/cinnamint/com/phonesaber/autostart.java
+++ app/src/main/java/cinnamint/com/phonesaber/autostart.java
@@ -1,18 +1,17 @@
-//package cinnamint.com.phonesaber;
-//
-//import android.content.BroadcastReceiver;
-//import android.content.Context;
-//import android.content.Intent;
-//import android.util.Log;
-//import android.widget.Toast;
-//
-//public class autostart extends BroadcastReceiver {
-//
-//    public void onReceive(Context context, Intent arg1) {
-//        Log.d("WINSTON", "New BOOT");
-//        // Toast.makeText(context, "RECEIVED", Toast.LENGTH_LONG).show();
-//        Intent intentStartService = new Intent(context, UpdateService.class);
-//        // No need for put extra, default screenOff = false
-//        context.startService(intentStartService);
-//    }
-//}
+package cinnamint.com.phonesaber;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class autostart extends BroadcastReceiver {
+
+    public void onReceive(Context context, Intent arg1) {
+        Log.d("WINSTON", "New BOOT");
+        Intent intentStartService = new Intent(context, UpdateService.class);
+        // No need for put extra, default screenOff = false
+        context.startService(intentStartService);
+    }
+}
